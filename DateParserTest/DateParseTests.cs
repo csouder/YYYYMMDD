955e24bec67e8a57f33432c205c0419054adc962
csouder <77806064+csouder@users.noreply.github.com>
2021-01-21 15:56:58 -0500

    Add files via upload

---
+++ DateParserTest/DateParseTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using YYYYMMDD;
+
+namespace DateParserTest
+{
+    [TestClass]
+    public class DateParseTests
+    {
+
+        const char delimiter = '-';
+        [TestMethod]
+        public void DateTest()
+        {
+            string parseddate = "";
+            var date = new DateTime(2020, 6, 1);
+            parseddate = YYYYMMDD.DataParser.GetIso8601DateString(date, delimiter);
+            Assert.AreEqual("2020-06-01", parseddate);
+        }
+
+        [TestMethod]
+        [TestCategory("NULLs")]
+        public void Datewithnulldelimiter()
+        {
+            string parseddate = "";
+            var date = new DateTime(2021, 1, 20);
+            parseddate = YYYYMMDD.DataParser.GetIso8601DateString(date);
+            Assert.AreEqual("20210120", parseddate);
+        }
+
+        [TestMethod]
+        [DataRow(2, 3, 4, "00020304")]
+        [DataRow(2, 3, 4, "0002-03-04", '-')]
+        [DataRow(12, 12, 26, "00121226")]
+        [DataRow(312, 12, 26, "03121226")]
+        [DataRow(2020, 3, 9, "20200309")]
+        public void LessthanFullDigits(int year, int month, int day, string expected, char? delimiter = null)
+        {
+            var parseddate = "";
+            var date = new DateTime(year, month, day);
+            if (string.IsNullOrEmpty(delimiter.ToString()))
+                parseddate = YYYYMMDD.DataParser.GetIso8601DateString(date);
+            else
+                parseddate = YYYYMMDD.DataParser.GetIso8601DateString(date, delimiter);
+            Assert.AreEqual(expected, parseddate);
+        }
+    }
+}
---
+++ YYYYMMDD/DateParse.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace YYYYMMDD
+{
+    public class DataParser
+    {
+        private const short DAY = 1;
+        private const short MONTH = 2;
+        private const short YEAR = 3;
+
+        /// <summary>
+        /// Returns leading zeros to date as necessary to make true YYYYMMDD format
+        /// </summary>
+        /// <example>
+        /// LeadingZeros(timenum, timetype) -> "000"
+        /// </example>
+        /// <param name="timenum">Value of the date component.</param>
+        /// <param name="timetype">Represents date component. 1=DAY, 2=MONTH, 3=YEAR</param>
+        /// <returns>The appropirate amount of leading zeros.</returns>
+        private static string LeadingZeros(int timenum, short timetype)
+        {
+            string zeros = "";
+            switch (timetype)
+            {
+                case DAY:
+                case MONTH:
+                    if (timenum < 10)
+                        zeros = "0";
+                    break;
+                case YEAR:
+                    if (timenum < 10)
+                        zeros += "000";
+                    else if (timenum < 100)
+                        zeros += "00";
+                    else if (timenum < 1000)
+                        zeros += "0";
+                    break;
+            }
+            return zeros;
+        }
+
+        /// <summary>
+        /// Returns an ISO 8601 date string using the specified delimiter.
+        /// </summary>
+        /// <example>
+        /// GetIso8601DateString(date) -> "YYYYMMDD"
+        /// GetIso8601DateString(date, '-') -> "YYYY-MM-DD"
+        /// </example>
+        /// <param name="date">The date to format.</param>
+        /// <param name="delimiter">The delimiter character or null if no
+        /// delimiter should be used.</param>
+        /// <returns>The formatted date.</returns>
+        public static string GetIso8601DateString(DateTime date, char? delimiter = null)
+        {
+            if(date == null)
+            {
+                throw new ArgumentNullException("date");
+            }
+            string formatteddate = "";
+            if (string.IsNullOrEmpty(delimiter.ToString()))
+            {
+                formatteddate = LeadingZeros(date.Year, YEAR)
+                    + date.Year.ToString()
+                    + LeadingZeros(date.Month, MONTH)
+                    + date.Month.ToString()
+                    + LeadingZeros(date.Day, DAY)
+                    + date.Day;
+            }
+            else
+            {
+                formatteddate = LeadingZeros(date.Year, YEAR)
+                    + date.Year.ToString() + delimiter
+                    + LeadingZeros(date.Month, MONTH)
+                    + date.Month.ToString() + delimiter
+                    + LeadingZeros(date.Day, DAY)
+                    + date.Day;
+            }
+            return formatteddate;
+        }
+    }
+}
